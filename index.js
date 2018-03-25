01fd19675833a2d789fe1593f9cfc5f4ffefa8f6
Welder Luz <wweellddeerr@gmail.com>
2018-03-24 22:25:27 -0300

    autenticação oauth google configurada e retornando corretamente

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys')
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ oi: 'mundo' });
-});
+passport.use(new GoogleStrategy(
+  {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  },
+  acessToken => {
+    console.log(acessToken)
+  }
+));
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
