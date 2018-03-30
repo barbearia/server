c69c665498c4932812aad29c8473e5f8036ad3c6
Welder Luz <wweellddeerr@gmail.com>
2018-03-29 22:08:44 -0300

    adicionei endpoint para logout e outro para verificar usuário logado

--- services/routes.js
+++ services/routes.js
@@ -9,5 +9,17 @@ module.exports = (app) => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  /**
+   * O método logout é adicionado automaticamente ao objeto req pelo passport.
+   */
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  })
+
+  app.get('/api/usuario', (req, res) => {
+    res.send(req.user)
+  });
 }
 
