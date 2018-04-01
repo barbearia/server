f3e1e76b20c35cd8b92357b14f1503752de83f96
Welder Luz <wweellddeerr@gmail.com>
2018-04-01 16:28:20 -0300

    Configuração inicial do react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Clientes = () => <h2>Lista de Clientes</h2>;
+const Cliente = () => <h2>Novo Cliente</h2>;
+const Home = () => <h2>Home</h2>;
 
 const App = () => {
   return (
-    <div>Oi mundo</div>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={Home} />
+          <Route path="/clientes" exact component={Clientes} />
+          <Route path="/clientes/novo" component={Cliente} />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
