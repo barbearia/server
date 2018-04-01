e18a7902a2c58a8fcedd1dc3acac0931d8e0d185
Welder Luz <wweellddeerr@gmail.com>
2018-04-01 09:02:56 -0300

    configuração inicial do redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/**
+ * Essa tag é uma espécie de "glue code" entre o react e o redux.
+ */
+import { Provider } from 'react-redux';
+
+/**
+ * Helpers do redux.
+ */
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+/**
+ * O primeiro argumento é uma lista de reducers.
+ *
+ * O segundo é relacionado ao estado inicial da aplicação
+ * e possui maior utilidade quando se utiliza SSR.
+ */
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
