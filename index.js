18a77a9080fa511e07b05ebcdd299c67ef2511c3
Welder Luz <wweellddeerr@gmail.com>
2018-04-02 10:23:38 -0300

    configuração para disponibilizar a aplicação cliente por meio do express

--- index.js
+++ index.js
@@ -19,5 +19,20 @@ app.use(passport.session());
 
 require('./services/routes')(app);
 
+if(process.env.NODE_ENV === 'production') {
+  /**
+   * onde procurar os arquivos .js e .css
+   */
+  app.use(express.static('/client/build'));
+
+  /**
+   * qualquer request que não possua rota definida irá retornar o index.html
+   */
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
